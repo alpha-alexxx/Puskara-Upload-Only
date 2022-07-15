981d13190f2fdc10b2922c1416ee20a1d5640277
alpha-alexxx <65218056+alpha-alexxx@users.noreply.github.com>
2022-07-15 08:20:09 +0530

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -69,7 +69,7 @@ class Uploader:
             )
             description = (
                 Config.VIDEO_DESCRIPTION
-                + "\nUploaded to YouTube with https://tx.me/utubeitbot"
+                + "Thank You for watching my channel.\n Like, Share and Subscribe"
             )[:5000]
             if not Config.UPLOAD_MODE:
                 privacyStatus = "private"
