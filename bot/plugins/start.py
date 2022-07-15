2c172daa2b7b251832e3ba48cb78361325c40a2e
alpha-alexxx <65218056+alpha-alexxx@users.noreply.github.com>
2022-07-15 07:53:38 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -5,6 +5,7 @@ from ..translations import Messages as tr
 from ..config import Config
 from ..utubebot import UtubeBot
 
+
 @UtubeBot.on_message(
     Filters.private
     & Filters.incoming
@@ -18,6 +19,15 @@ async def _start(c: UtubeBot, m: Message):
         quote=True,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup([
-             [InlineKeyboardButton("Visit My Website", url="https://lethargicproj.blogspot.com/")]
+                  [
+                      InlineKeyboardButton("How To Use Me🙄", callback_data="/help")
+                  ],
+                  [
+                      InlineKeyboardButton("Project Channel!", url="https://t.me/hxbots"),
+                      InlineKeyboardButton("Support Group", url="https://t.me/HxSupport")
+                  ],
+                  [  
+                      InlineKeyboardButton("Buy Me A Tea", url="https://upayme.vercel.app/kkirodewal@ybl")
+                  ]]
         ),
     )
